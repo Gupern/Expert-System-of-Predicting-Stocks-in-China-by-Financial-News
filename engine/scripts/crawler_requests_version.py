a68866af1c4ad29108f0c5530f195efb5f888e9b
root <root@localhost.localdomain>
2017-11-04 21:39:54 -0400

    优化了爬虫

--- engine/scripts/crawler_requests_version.py
+++ engine/scripts/crawler_requests_version.py
@@ -64,67 +64,116 @@ def fetch(url, retry=0):
     return None
 
 # 解析详情页数据
-def parse_detail(detail_urls):
-    res = []
-    for detail_url in detail_urls:
-        source_html = fetch(detail_url)
-        selector = etree.HTML(source_html)
-        # ''.join将list转为str
-        title = ''.join(selector.xpath('//div[@class="layout mg articleName"]/h1/text()'))
-        if not title:
-            title = ''.join(selector.xpath('//div[@class="art_title"]/h1/text()'))
-        body = ''.join(selector.xpath('//div[@class="art_contextBox"]/p/descendant::text()'))
-        if not body:
-            body = ''.join(selector.xpath('//div[@class="art_context"]/p/descendant::text()'))
-        date = ''.join(selector.xpath('//span[@class="pr20"]/text()'))
-        if not date:
-            date = ''.join(selector.xpath('//span[@id="artibodyDesc"]/span[1]/text()'))
-        
-        tmp = {'title':title.strip(), 
-               'body':body.strip(),
-               'date':date.strip(),
-               'source_url':detail_url.strip()
-               }
-        res.append(tmp)
+def parse_detail(db_name, detail_urls_col_name, target_col_name, floor, celling):
+    detail_urls_col = get_col(db_name, detail_urls_col_name)
+    # 接收从floor到celling的参数，便于多进程爬取
+    cursor = detail_urls_col.find().skip(floor).limit(celling)
 
-#        break
+    for i in cursor:
+        try:
+            detail_url = i['detail_url']
+            print('parsing: ' + detail_url)
+            source_html = fetch(detail_url)
+            selector = etree.HTML(source_html)
+            # ''.join将list转为str
+            title = ''.join(selector.xpath('//div[@class="layout mg articleName"]/h1/text()'))
+            if not title:
+                title = ''.join(selector.xpath('//div[@class="art_title"]/h1/text()'))
+            body = ''.join(selector.xpath('//div[@class="art_contextBox"]/p/descendant::text()'))
+            if not body:
+                body = ''.join(selector.xpath('//div[@class="art_context"]/p/descendant::text()'))
+            date = ''.join(selector.xpath('//span[@class="pr20"]/text()'))
+            if not date:
+                date = ''.join(selector.xpath('//span[@id="artibodyDesc"]/span[1]/text()'))
+            
+            res = {'title':title.strip(), 
+                   'body':body.strip(),
+                   'date':date.strip(),
+                   'source_url':detail_url.strip()
+                   }
+            store(db_name, target_col_name, res)
+        except:
+            pass
 
-    return(res)
+
 
 # 解析列表页数据
-def parse_list():
+def parse_list(db_name, detail_urls_col_name):
+    print('parsing')
     pages = list(range(1,2057))
     urls = ['http://stock.hexun.com/stocknews/index-{}.html'.format(page) \
             for page in pages]
     urls.append('http://stock.hexun.com/stocknews/index.html')
 
-    detail_urls = []
     for url in urls:
         source_html = fetch(url)
         selector = etree.HTML(source_html)
         detail_url = selector.xpath('//div[@class="temp01"]/ul/ul/li/a/@href')
         for tmp in detail_url:
-            detail_urls.append(tmp)
-#        break # 正式跑的时候注释掉
+            print(tmp)
+            res = {'detail_url': tmp}
+            print(res)
+            print(type(res))
+            store(db_name, detail_urls_col_name, res) # 可以用bulk 和 yield优化
+    #        break # 正式跑的时候注释掉
 
-    return detail_urls
+# 连接mongodb的col
+def get_col(db_name, col_name, ip='0.0.0.0',port=27017):
+    client = MongoClient() # 以后慢慢优化
+    db = client[db_name]
+    col = db[col_name]
+    return col
 
 # 存储数据
-def store(res):
-    client = MongoClient() # default is 127.0.0.1:27017
-    db = client['stock_news']
-    col = db['hexun']
-    for item in res:
-        if not col.find_one({'source_url':item['source_url']}):
-            col.insert(item)
+def store(db_name, col_name, item):
+    print('storing')
+    col = get_col(db_name, col_name)
+    # 去重,直接find item即可，_id无关
+    if not col.find_one(item):
+        col.insert(item)
 
 # 主函数
 def main():
-    detail_urls = parse_list()
-    res = parse_detail(detail_urls)
-    for i in res:
-        print(i)
-    store(res)
+    db_name = 'stock_news' 
+    detail_urls_col_name = 'detail_urls'
+    target_col_name = 'stock.hexun.com'
+
+##############################################
+# 这里可以优化成一个输入界面，让用户选择
+# 1. 爬取详情页链接
+# 2. 爬取详情页内容 
+# 3. 全部爬取 
+##############################################
+    print('请选择您想要进行的操作:\n')
+    print('1. 爬取详情页链接\n')
+    print('2. 爬取详情页内容\n')
+    print('3. 全部爬取\n')
+    print('0. 退出\n')
+    choice = int(input())
+
+    if choice==0:
+        pass
+    elif choice==1:
+        # 爬取详情页链接
+        parse_list(db_name, detail_urls_col_name)
+    elif choice==2:
+        # 爬取详情页内容 
+        floor = int(input('请输入从第几个开始爬'))
+        celling = int(input('请输入到第几个结束'))
+        parse_detail(db_name, detail_urls_col_name, target_col_name, floor, celling)
+    elif choice==3:
+        # 爬取详情页链接
+        parse_list(db_name, detail_urls_col_name)
+
+        # 爬取详情页内容 
+        floor = int(input('请输入从第几个开始爬'))
+        celling = int(input('请输入到第几个结束'))
+        parse_detail(db_name, detail_urls_col_name, target_col_name, floor, celling)
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
