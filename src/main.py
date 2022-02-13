eeab7d9fb6145b7d21c882d210badd74c1305bef
Gupern <guperner@hotmail.com>
2022-02-14 01:39:53 +0800

    feat: 搭建框架模块

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+"""
+    author: Gupern
+    description: this is the main file, which can using command line to call modules' function, such as engine, script, source, etc. 
+    using: 
+        1. execute crawler script: `python main.py crawl script/a_stocks_crawler.py`
+        2. 
+"""
